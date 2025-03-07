074b952d5ecc5f3b2d66d006f687f00726dbcc8a
Akash290104 <mishra.ak801@gmail.com>
2025-03-07 20:51:36 +0530

    added vercel url in cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: [
+    "http://localhost:3000",
+    "https://final-chat-application-xi.vercel.app",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
